24a5a955be90f0d77595600ca4921b2891f8ddf4
tomahim <thimblot@gmail.com>
2026-03-21 22:39:18 +0100

    changes

--- three-js-labs/scenes/test2.js
+++ three-js-labs/scenes/test2.js
@@ -3,7 +3,7 @@ import { BoxGeometry, MeshStandardMaterial, Mesh } from 'three';
 export function createScene(scene) {
   // Create a cube
   const geometry = new BoxGeometry(1, 1, 1);
-  const material = new MeshStandardMaterial({ color: 0x00ff00 });
+  const material = new MeshStandardMaterial({ color: 'red' });
   const cube = new Mesh(geometry, material);
   scene.add(cube);
   
